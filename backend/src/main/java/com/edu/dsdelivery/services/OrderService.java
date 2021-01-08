36f6099cfbf63c9e6ebb8230ef5f9862c3216929
Caio Vitor <caiocogomes@hotmail.com>
2021-01-07 21:50:28 -0300

    GET/ orders

--- backend/src/main/java/com/edu/dsdelivery/services/OrderService.java
+++ backend/src/main/java/com/edu/dsdelivery/services/OrderService.java
@@ -1,8 +1,25 @@
 package com.edu.dsdelivery.services;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.edu.dsdelivery.dto.OrderDTO;
+import com.edu.dsdelivery.entities.Order;
+import com.edu.dsdelivery.repositories.OrderRepository;
 
 @Service
 public class OrderService {
-
+	
+	@Autowired
+	private OrderRepository repository;
+	
+	@Transactional(readOnly = true)
+	public List<OrderDTO> findAll(){		
+		List<Order> list = repository.findOrdersWithProducts();
+		return list.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList());
+	}
 }
