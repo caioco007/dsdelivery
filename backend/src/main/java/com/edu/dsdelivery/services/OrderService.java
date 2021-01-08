5808f33de3f4ab5958db9207877c0bb795dd8072
Caio Vitor <caiocogomes@hotmail.com>
2021-01-07 23:02:36 -0300

    PUT/ orders/{id}/delivered

--- backend/src/main/java/com/edu/dsdelivery/services/OrderService.java
+++ backend/src/main/java/com/edu/dsdelivery/services/OrderService.java
@@ -31,7 +31,7 @@ public class OrderService {
 		return list.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList());
 	}
 	
-	@Transactional()
+	@Transactional
 	public OrderDTO insert(OrderDTO dto){	
 		Order order = new Order(null,dto.getAddress(),dto.getLatitude(),dto.getLongitude(),
 				Instant.now(),OrderStatus.PENDING);
@@ -42,6 +42,13 @@ public class OrderService {
 		}
 		order = repository.save(order);
 		return new OrderDTO(order);
-				
+	}
+	
+	@Transactional
+	public OrderDTO setDelivered(Long id){	
+		Order order = repository.getOne(id);
+		order.setStatus(OrderStatus.DELIVERED);
+		order = repository.save(order);
+		return new OrderDTO(order);
 	}
 }
